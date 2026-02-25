d0374f17c3e317ec72d0b0620f3a274390d77cf2
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2026-02-25 17:42:19 +0530

    github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.example.com"
+  site: 'https://qu-ack.github.io',
+  base: "/me"
 });
